741e662f4011918108ffead95c9b79f4adac5cda
Anton Khelou <a.khelou@gmail.com>
2014-10-24 02:42:25 -0400

    Update Thread and Comment models
    In Thread:
    -Rename the poster field to thread_poster
    In Comment:
    -Add comment_poster field associated to auth.User model
    -Add related_name to thread field for reverse relationship referencing
    -Add datetime_modified field
    -Add auto_now_add=True field to datetime_posted

--- proj599/models.py
+++ proj599/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Thread(models.Model):
-    poster = models.ForeignKey('auth.User', null=True, blank=True, related_name='threads')
+    thread_poster = models.ForeignKey('auth.User', null=True, blank=True, related_name='posted_threads')
     subject = models.CharField(max_length=144)
     num_upvotes = models.IntegerField(default=0)
     num_views = models.IntegerField(default=0)
@@ -16,8 +16,10 @@ class Thread(models.Model):
 
 
 class Comment(models.Model):
-    thread = models.ForeignKey(Thread)
-    parent_comment = models.OneToOneField("Comment", null=True, blank=True)
-    datetime_posted = models.DateTimeField()
+    comment_poster = models.ForeignKey('auth.User', null=True, blank=True, related_name='posted_comments')
+    thread = models.ForeignKey(Thread, related_name='thread_comments')
+    parent_comment = models.ForeignKey("Comment", null=True, blank=True)
+    datetime_posted = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
     num_upvotes = models.IntegerField()
     message = models.CharField(max_length=10000, blank=True)
