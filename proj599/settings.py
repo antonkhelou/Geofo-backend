af66134de7fea8eec3afd221c9f4f5370d467fa7
Anton Khelou <a.khelou@gmail.com>
2014-12-02 22:54:08 -0500

    Add pagination
    SWAG

--- proj599/settings.py
+++ proj599/settings.py
@@ -65,6 +65,12 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'PAGINATE_BY': 2,                 # Default to 10
+    'PAGINATE_BY_PARAM': 'page_size',  # Allow client to override, using `?page_size=xxx`.
+    'MAX_PAGINATE_BY': 100             # Maximum limit allowed when using `?page_size=xxx`.
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
