7fac5e663bad50b69493f7799f7caa3bee92b9a1
Anton Khelou <a.khelou@gmail.com>
2014-12-14 22:01:57 -0500

    Add token authentication to server
    This commit adds a new POST hook:
    
    /api-token-auth/
    
    It requires both "username" and "password" attributes values in json
    format to be POSTed. If successfully authenticated, it will return a
    "token" value in json format with the token to be used. The token needs
    to be used with every HTTP request as follows:
    
    GET http://127.0.0.1:8000/api/example/ -H 'Authorization: Token
    9944b09199c62bcf9418ad846dd0e4bbdfc6ee4b'
    
    That is, the 'Authorization' HTTP header needs to be modified to
    include:
    
    Authorization: Token 9944b09199c62bcf9418ad846dd0e4bbdfc6ee4b
    
    With whitespaces separating the two strings.
    
    For more information, see:
    
    http://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication

--- proj599/models.py
+++ proj599/models.py
@@ -4,10 +4,18 @@ from django.contrib.auth.models import User
 from djcelery.models import PeriodicTask, IntervalSchedule
 from datetime import datetime, timedelta
 
-import settings
+from django.conf import settings
 from geopy.geocoders import Nominatim
 
-geolocator = Nominatim()
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
 class AppUser(models.Model):
--- proj599/urls.py
+++ proj599/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken import views as authtokenview
+
 from proj599 import views
 
 urlpatterns = patterns('',
@@ -14,6 +17,7 @@ urlpatterns = patterns('',
     url(r'^commentupvote/$', views.CommentUpvoteList.as_view()),
     url(r'^users/$', views.AppUserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.AppUserDetail.as_view(), name='user-detail'),
+    url(r'^api-token-auth/', authtokenview.obtain_auth_token),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
